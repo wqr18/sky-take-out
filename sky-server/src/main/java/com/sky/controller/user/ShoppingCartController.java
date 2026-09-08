4949a46b37c4d3551638c2d7adb3e9e0bc9a6d0f
wqr18 <3405224231@qq.com>
2026-09-08 16:08:27 +0800

    添加和查看购物车

---
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -0,0 +1,33 @@
+package com.sky.controller.user;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+import com.sky.result.Result;
+import com.sky.service.ShoppingCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user/shoppingCart")
+@Slf4j
+public class ShoppingCartController {
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    @PostMapping("/add")
+    public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO){
+        log.info("购物车数据：{}", shoppingCartDTO);
+        shoppingCartService.shoppingCartAdd(shoppingCartDTO);
+        return Result.success();
+    }
+
+    @GetMapping("/list")
+    public Result<List<ShoppingCart>> list(){
+        List<ShoppingCart> list =  shoppingCartService.showShoppingCart();
+        return Result.success(list);
+    }
+
+}
