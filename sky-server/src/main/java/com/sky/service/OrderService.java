ec6fa5d156ca5e2d7e85e4d2bcdf8d01b90724c9
wuqinrong <3405224231@qq.com>
2026-09-16 16:17:12 +0800

    商家查询订单详情、接单、拒单、取消订单

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,8 +1,6 @@
 package com.sky.service;
 
-import com.sky.dto.OrdersPageQueryDTO;
-import com.sky.dto.OrdersPaymentDTO;
-import com.sky.dto.OrdersSubmitDTO;
+import com.sky.dto.*;
 import com.sky.result.PageResult;
 import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderStatisticsVO;
@@ -28,4 +26,10 @@ public interface OrderService {
     PageResult conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO);
 
     OrderStatisticsVO statistics();
+
+    void confirm(OrdersConfirmDTO ordersConfirmDTO);
+
+    void rejection(OrdersRejectionDTO ordersRejectionDTO) throws Exception;
+
+    void cancel(OrdersCancelDTO ordersCancelDTO) throws Exception;
 }
